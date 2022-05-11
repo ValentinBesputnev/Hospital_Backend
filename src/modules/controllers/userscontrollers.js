825e9c237928e3e2c99a19cb8ca1c717fb32670f
ValentinBesputnev <besputnev.exceedteam@gmail.com>
2022-05-11 17:51:29 +0300

    created registration route

--- src/modules/controllers/userscontrollers.js
+++ src/modules/controllers/userscontrollers.js
@@ -1,7 +1,19 @@
 const User = require("../../schema/usersSchema");
+const bcrypt = require('bcryptjs');
 
-module.exports.getAllUsers = (req, res) => {
-  User.find().then(result => {
-    res.send({ data: result });
-  });
+module.exports.createNewUser = (req, res) => {
+  try {
+    const {username, password} = reg.body
+    const candidate = await User.findOne({username})
+    if (candidate) {
+      return res.status(400).json({message: "Пользователь с такой почтой зарегистрирован"})
+    }
+    const hashPassword = bcrypt.hashSync(password, 5);
+    const user = new User({username, password: hashPassword})
+    await user.save()
+    return res.json({message: "Пользователь успешно зарегистрирован"})
+  } catch(e) {
+    console.log(e);
+    res.status(400).json({messege: "Error"})
+  }
 };
