1401e26a1d41b9dcc06ca560da61c302be5c0fcd
ValentinBesputnev <besputnev.exceedteam@gmail.com>
2022-05-26 11:28:01 +0300

    fix login route

--- src/modules/controllers/usersControllers.js
+++ src/modules/controllers/usersControllers.js
@@ -1,52 +1,60 @@
-const User = require("../../schema/usersSchema");
 const bcrypt = require('bcryptjs');
-const { validationResult } = require("express-validator")
 const jwt = require('jsonwebtoken');
+const User = require("../../schema/usersSchema");
+const { validationResult } = require("express-validator")
 const secret = process.env.secret;
 
-const generateAccecToken = (id, username) => {
+const generateAccecToken = (_id, email) => {
   const payload = {
-    id,
-    username,
+    _id,
+    email,
   };
   return jwt.sign(payload, secret, { expiresIn: "24h" });
 };
 
-module.exports.createNewUser = (req, res) => {
-  try {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      return res.status(400).json({message: "Ошибка при регистрации", errors})
+module.exports.createNewUser = async (req, res) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    try {
+      const { email, password } = req.body;
+      const candidate = await User.findOne({ email });
+      if (candidate) {
+        return res
+          .status(400)
+          .json({ message: "Пользователь с такой почтой зарегистрирован" });
+      }
+
+      const hashPassword = bcrypt.hashSync(password, 7);
+
+      const user = new User({ email, password: hashPassword });
+
+      await user.save();
+      const token = generateAccecToken(user._id, email);
+      return res.json({ token, email });
+    } catch (error) {
+      res.status(400).json({ message: "Registration error" });
     }
-    const {username, password} = reg.body
-    const candidate = await User.findOne({username})
-    if (candidate) {
-      return res.status(400).json({message: "Пользователь с такой почтой зарегистрирован"})
-    }
-    const hashPassword = bcrypt.hashSync(password, 5);
-    const user = new User({username, password: hashPassword})
-    await user.save()
-    return res.json({message: "Пользователь успешно зарегистрирован"})
-  } catch(e) {
-    console.log(e);
-    res.status(400).json({messege: "Error"})
+  } else {
+    return res.status(400).json({ message: errors });
   }
 };
 
-module.exports.loginUser = (req,res) => {
+module.exports.loginUser = async (req, res) => {
   try {
-    const {username, password} = reg.body
-    const user = await User.findOne({username})
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({message: `Пользователь ${username} не найден`})
+      return res
+        .status(400)
+        .json({ message: `Пользователь ${email} не найден` });
     }
-    const validPassword = bcrypt.compareSync(password, user.password)
+    const validPassword = bcrypt.compareSync(password, user.password);
     if (!validPassword) {
-      return res.status(400).json({message: "Введен неверный пароль"})
+      return res.status(400).json({ message: "Введен неверный пароль" });
     }
-    const token = generateAccecToken(user._id, username)
+    const token = generateAccecToken(user._id, email);
+    return res.json({ token, email });
   } catch (e) {
-    console.log(e);
-    res.status(400).json({message: "Login error"})
+    res.status(400).json({ message: "Login error" });
   }
-}
+};
